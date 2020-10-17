79111a86edc78b99902c1706c31f236d443e79c2
Kevin Damen <k.damen@bellt.nl>
2020-10-17 16:14:53 +0200

    Add project files.

---
+++ FindstrUI.Core/TerminalCommand.cs
@@ -0,0 +1,44 @@
+﻿using System.Diagnostics;
+
+namespace FindstrUI.Core
+{
+    public class TerminalCommand
+    {
+        /// <summary>
+        /// Executes a shell command and get the result.
+        /// </summary>
+        /// <param name="command">string command</param>
+        /// <returns>string, as output of the command.</returns>
+        public static string GetResult(string command)
+        {
+            var procStartInfo = GetProcessStartInfo(command);
+
+            // Now we create a process, assign its ProcessStartInfo and start it
+            Process proc = new Process() { StartInfo = procStartInfo };
+            proc.Start();
+
+            // Return the output into a string
+            return proc.StandardOutput.ReadToEnd();
+        }
+
+        // todo: Read result from stream
+        // Get every result seperately.
+
+        private static ProcessStartInfo GetProcessStartInfo(string command)
+        {
+            // create the ProcessStartInfo using "cmd" as the program to be run, and "/c " as the parameters.
+            // Incidentally, /c tells cmd that we want it to execute the command that follows, and then exit.
+            ProcessStartInfo procStartInfo = new ProcessStartInfo("cmd", "/c " + command)
+            {
+                // The following commands are needed to redirect the standard output. 
+                //This means that it will be redirected to the Process.StandardOutput StreamReader.
+                RedirectStandardOutput = true,
+                UseShellExecute = false,
+                // Do not create the black window.
+                CreateNoWindow = true
+            };
+
+            return procStartInfo;
+        }
+    }
+}
---
+++ FindstrUI.WPF/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+using FindstrUI.Core;
+using FindstrUI.WPF.Util;
+
+namespace FindstrUI.WPF.ViewModels
+{
+    class MainWindowViewModel : NotifyPropertyChanged
+    {
+        public string CommandResult 
+        { 
+            get => _commandResult;
+            set
+            {
+                if (_commandResult == value)
+                    return;
+
+                _commandResult = value;
+                OnPropertyChanged();
+            }
+        }
+        public string SearchString { get; set; }
+
+        public void ExecuteCommand()
+        {
+            if (string.IsNullOrEmpty(SearchString))
+            {
+                CommandResult = "The search string cannot be empty.";
+                return;
+            }
+
+            CommandResult = "Wait for result...";
+
+            var worker = new BackgroundWorker();
+            worker.DoWork += (object sender, DoWorkEventArgs e) => { e.Result = TerminalCommand.GetResult((string)e.Argument); };
+            worker.RunWorkerCompleted += (object sender, RunWorkerCompletedEventArgs e) => { CommandResult = (string)e.Result; };
+            worker.RunWorkerAsync(@$"findstr /p /i /n ""{SearchString}"" ""E:\Share\sm_reports\*""");
+        }
+
+        private string _commandResult;
+    }
+}
